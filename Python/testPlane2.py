43e80ada2dfd816de247490228dbb72f2708e7bd
Tinnabhand Patana-anake <tintin.thetintinman@gmail.com>
2018-02-06 01:20:31 -0800

    add udp listener function

--- Python/testPlane2.py
+++ Python/testPlane2.py
@@ -2,12 +2,18 @@ from __future__ import print_function
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 from pymavlink import mavutil
 import time
-
-
-# Set up option parsing to get connection string
 import argparse
+import socket
+import re
 
+# Define variables
 CUSTOM_TRAPIS_ID = 999
+UDP_IP_ADDRESS = "127.0.0.1"
+UDP_PORT_NO = 15000
+
+''' -------------------------------------------------------------------
+        Set up option parsing to get connection string
+---------------------------------------------------------------------'''
 
 parser = argparse.ArgumentParser(description="Commands vehicle using vehicle.simple_goto.")
 parser.add_argument("--connect",
@@ -18,37 +24,51 @@ connection_string = args.connect
 
 
 ''' -------------------------------------------------------------------
+                        Set up UDP Listener
+---------------------------------------------------------------------'''
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+
+
+''' -------------------------------------------------------------------
                           Connect
 ---------------------------------------------------------------------'''
 
-# Connect to the Vehicle (in this case a simulator running the same computer)
-#vehicle = connect(connection_string, wait_ready=True)
+# Connect to the vehicle
 vehicle = connect(connection_string, wait_ready=False)
-
-@vehicle.on_message('CUSTOM_TRAPIS')
-def listener(self, name, message):
-    print(message)
 
 
 ''' ------------------------------------------------------------------
                             MAVLINK Message
 --------------------------------------------------------------------'''
-#mavutil.mavlink.MAV_CMD_DO_SET_ROI
-#msg = vehicle.message_factory.custom_trapis_encode(123,321,99)
-msg = vehicle.message_factory.command_long_encode(
+# Regular expression pattern
+# Location: (lat)N (lon)W (alt)
+regex = r"[A-Za-z]+[:]\s([0-9]+[\.][0-9]+)[N]\s([0-9]+[\.][0-9]+)[W]\s([0-9]+[\.][0-9]+)"
+
+while True:
+    # Listen for data from UDP
+    data, addr = serverSocket.recvfrom(1024)
+    messageReceived = data.decode("hex")
+    match = re.search(regex, messageReceived)
+    lat = float(match.group(1))
+    lon = float(match.group(2))
+    alt = float(match.group(3))
+
+    # Packing Mavlink message
+    msg = vehicle.message_factory.command_long_encode(
     0, 0,    # target system, target component
     CUSTOM_TRAPIS_ID, #command
     0, #confirmation
-    123,    
-    123,
-    1,
+    lat,
+    lon,
+    alt,
     0,
     0, 0, 0)
 
-# send command to vehicle
-vehicle.send_mavlink(msg)
-print("Custom Trapis Mavlink sent")
-time.sleep(5)
+    # Send command to vehicle
+    vehicle.send_mavlink(msg)
+    print("Custom Trapis Mavlink sent")
 
 
 ''' ------------------------------------------------------------------
